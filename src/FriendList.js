62c489de01b8d7efc486330658d2650f323b47a8
rattanakorn00 <rattanakorn.plan@bumail.net>
2022-02-26 08:49:07 +0700

    merge_class07

---
+++ src/FriendList.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './FriendList.css';
+import App from './components/App';
+import Avatar from './components/Avatar';
+
+function FriendList(){
+  const [friends, setFriends] = useState([
+    {user: "user1", displayName: "ไอ้ปื้ด"},
+    {user: "user2", displayName: "นางสาวส้ม"},
+    {user: "user3", displayName: "พลทหารแดง"},
+    {user: "user4", displayName: "ศาตราจารย์นายกรัฐมนตรีดอกเตอร์เขียว"},
+  ]);
+
+  return(
+    <App>
+      <div className="friend-list">
+       ใส่โค้ดที่นี่
+      </div>
+    </App>
+  )
+}
+
+export default FriendList;
